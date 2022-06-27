f48cb3f3d170dcfb29706cd8959a3373c2a0efb9
WinsomeQuill <artemqa.belyakov07@gmail.com>
2022-06-27 21:20:14 +0700

    Refactoring

--- target/debug/deps/calc-5a5ce68d8af90326.d
+++
@@ -1,5 +0,0 @@
-d:\Rust\calc\target\debug\deps\calc-5a5ce68d8af90326.rmeta: src\main.rs
-
-d:\Rust\calc\target\debug\deps\calc-5a5ce68d8af90326.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/calc-929a923eb1cd3e98.d
+++
@@ -1,5 +0,0 @@
-d:\Rust\calc\target\debug\deps\calc-929a923eb1cd3e98.rmeta: src\main.rs
-
-d:\Rust\calc\target\debug\deps\calc-929a923eb1cd3e98.d: src\main.rs
-
-src\main.rs:
