c1b1e569820fe1b292427b77636552c682c437e2
WinsomeQuill <artemqa.belyakov07@gmail.com>
2022-06-27 21:19:03 +0700

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::io::{self, Read};
+use std::io::{
+    self,
+    Read
+};
 use std::num::ParseIntError;
+use std::io::{
+    stdin,
+    stdout,
+    Write
+};
 
 fn input() -> String {
-    use std::io::{stdin,stdout,Write};
-    let mut s=String::new();
-    let _=stdout().flush();
+    let mut s = String::new();
+    let _ = stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
     if let Some('\n')=s.chars().next_back() {
         s.pop();
@@ -12,44 +19,46 @@ fn input() -> String {
     if let Some('\r')=s.chars().next_back() {
         s.pop();
     }
-    return s;
+    s
 }
 
 fn main() {
-    let mut operator;
+    let mut operator: String;
     loop {
         print!("Введите A: ");
 
         let a = match input().parse::<i32>() {
             Ok(a) => a,
-            Err(error) => { println!("А может быть только числом!"); continue; },
+            Err(error) => {
+                println!("А может быть только числом!");
+                continue;
+            },
         };
 
         print!("Введите B: ");
         let b = match input().parse::<i32>() {
             Ok(b) => b,
-            Err(error) => { println!("B может быть только числом!"); continue; },
+            Err(error) => {
+                println!("B может быть только числом!");
+                continue;
+            },
         };
 
         print!("Введите Оператор: ");
         operator = input();
 
-        if operator == "+" {
-            println!("{}", a+b);
-        } else {
-            if operator == "-" {
-                println!("{}", a-b);
-            } else {
-                if operator == "*" {
-                    println!("{}", a*b);
-                } else {
-                    if operator == "/" {
-                        println!("{}", a/b);
-                    } else {
-                        println!("Такого оператора нету!");
-                    }
+        match operator.as_str() {
+            "+" => println!("{}", a + b),
+            "-" => println!("{}", a - b),
+            "*" => println!("{}", a * b),
+            "/" => {
+                if a == 0 {
+                    println!("Делить на ноль нельзя!");
+                    continue;
                 }
-            }
-        } 
-    }
+                println!("{}", a / b);
+            },
+            _ => println!("Такого оператора нету!"),
+        };
+    };
 }
